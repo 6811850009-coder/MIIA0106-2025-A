f06a48522df86b64e9ef199ed5291de587ab7ea6
Pumipat Thitijaratwasukun <6811850009@mut.ac.th>
2025-11-25 16:54:41 +0700

    TEST

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "Hello lab2\n";
+    std::cout << "Hello lab2 olo\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
